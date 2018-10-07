d9625dbc611e3d37e3a3deefe39c4676bbf220b1
MAC <mac@MACs-MacBook-Pro.local>
2018-10-07 06:19:41 +0100

    initial commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by MAC on 10/7/18.
+//  Copyright © 2018 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func showAlert()
+    {
+        print("Hello!")
+    }
+}
+
